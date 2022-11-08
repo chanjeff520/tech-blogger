27cb36837667010d13047971565d368b51085be8
chanjeff520 <chanjeff520@gmail.com>
2022-11-08 01:16:02 -0500

    add seeds to comments

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ const Comment = require('./Comment')
 //user association with blog
 User.hasMany(Blog, {foreignKey: 'user_id'});
 
-//blog association with  comment
+//blog association with comment
 Blog.hasMany(Comment, {foreignKey: 'blog_id'});
 
+//user association with comment
 User.hasMany(Comment, {foreignKey: 'user_id'});
---
+++ seeds/commentData.js
@@ -0,0 +1,20 @@
+const { join } = require('path')
+const {Comment} = require('../models')
+
+const commentData = [
+    {
+        text: "So this is my interpretation of MVC",
+        user_id: 1,
+        blog_id: 1
+    },
+    {   
+        text: "If anyone have an question please reply in the comments",
+        user_id: 1,
+        blog_id: 1
+    },
+    {
+        text: "This was very interesting, thank you for sharing!!",
+        user_id: 2,
+        blog_id: 1,
+    }
+]
